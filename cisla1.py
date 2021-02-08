4cb623c68bf7243c671cbb2e160b002b4572f5e5
Jan Halenka <halenka.jan@gmail.com>
2021-02-08 22:31:41 +0100

    Secte prirozena cisla od 1 do zadaneho

--- cisla1.py
+++ cisla1.py
@@ -3,4 +3,10 @@ while True:
     if n > 0:
         break
 
-print("Zadane cislo je", n)
+print('Zadane cislo je ' + (str)(n))
+
+soucet = 0
+for i in range(n+1):
+    soucet += i
+print("Soucet prvnich n prirozenych cisel je: " + (str)(soucet))
+
