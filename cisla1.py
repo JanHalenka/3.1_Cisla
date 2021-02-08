384acd8086a10549f46012698688ab5682bb20a3
Jan Halenka <halenka.jan@gmail.com>
2021-02-08 22:56:38 +0100

    Doplneny komentare

--- cisla1.py
+++ cisla1.py
@@ -1,10 +1,13 @@
+# Nacita cislo od uzivatele, predpoklada nacteni int, zatim neosetreno
 while True:
     n = (int)(input("Zadej prirozene cislo: "))
     if n > 0:
         break
 
+# Vypise zadane cislo
 print('Zadane cislo je ' + (str)(n))
 
+# Secte prirozena cisla od 1 do n (zapocitava i realnou 0, coz zde ovsem nevadi)
 soucet = 0
 for i in range(n+1):
     soucet += i
